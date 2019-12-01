14157a8804f69aa449b349a4b03b330e63e32523
Rizac1 <55118545+Rizac1@users.noreply.github.com>
2019-11-30 20:39:37 -0600

    second commit

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Home = () => (
+  <div>
+    <h2>Test Home page</h2>
+    <p>Lorem asdhasduaisdh asashdauispdhasui asdhausdhaisudh asuidhas uiashduias dhauisdhasui dhasui hasha dauishdauis dhaisudh asuidh asuidasuidh asidaush dauishdauis</p>
+  </div>
+)
+
