7de25a060c7a8103e6ec5aec3dba05733af3f77c
MichaelParker8 <54907898+MichaelParker8@users.noreply.github.com>
2019-12-03 12:25:42 -0600

    framework for project list

--- src/pages/NFPlist.js
+++ src/pages/NFPlist.js
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import NFPListMapping from '../components/NFPListMapping'
 export const NFPlist = () => (
+  // consturctor(){
+  //   this.state = {
+  //     nfplist: []
+  //   }
+  // }
   <div>
     <h2>Test Non-Profit List</h2>
+    <NFPListMapping nfplist={this.state.nfplist}  />
   </div>
 )
 
