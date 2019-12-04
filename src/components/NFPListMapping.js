e911d653846531fcd807bbaadfd7a9654a29e6c0
MichaelParker8 <54907898+MichaelParker8@users.noreply.github.com>
2019-12-04 09:27:55 -0600

    NFPList updates
    changed NFPList to class based component

--- src/components/NFPListMapping.js
+++ src/components/NFPListMapping.js
@@ -3,7 +3,7 @@ import NFPListIndividualFrame from './NFPListIndividualFrame'
 
 let NFPListMapping = (props) => {
     return props.nfplist.map((nfplist) => (
-        <NFPListIndividualFrame key={nfplist.id} nfplist={nfplist}/>
+        <NFPListIndividualFrame key={nfplist.email} nfplist={nfplist}/>
     ))
 }
 
