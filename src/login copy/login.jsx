dfe4b89a6b31765b9b3b938d272f34c9e56b8279
Will <williamwoods586@gmail.com>
2019-12-09 00:26:18 -0600

    updates to login

--- src/login copy/login.jsx
+++ src/login copy/login.jsx
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import {business} from "../login copy/assets/image-1.png"
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { BrowserRouter as Router, Switch, Route, useRouteMatch } from "react-router-dom";
@@ -84,34 +85,83 @@ export class Login extends React.Component {
     const sidebarCollasped = windowWidth < 1100;
 
     return (
-     
-        <div className="login">      {/*Background image.  UI looks better without*/} {/* <div data-layer="1b95d00d-ebc6-44ef-a711-96b9158f9cef" className="image-1"></div>
-        */}
-        <div class="row">
-            <div className="log-in-to-outreachd">Log in to OutReachD<br /><br /></div>
-            <form onSubmit={this.handleSubmit}>
-              <input type="text" placeholder="email" value={this.state.name} onChange={this.handleChange} className="rectangle-1" />
-              <input type={isPasswordShown ? "text" : "password"} placeholder="password" value={this.state.name} className="rectangle-2" />
-              <i className={`fa ${isPasswordShown ? "fa-eye" : "fa-eye-slash"} password-icon`} onClick={this.togglePasswordVisiblity} />
+      <div class="container">
+        {/*Nav */}
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">OUTREACHD</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarColor02">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Non-Profile List</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Profile</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="text" placeholder="Search" />
+              <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
             </form>
-            <Link to={"./NFPList"}>
-              <button className="rectangle-3">Log in </button></Link>
-            <input type="checkbox" className="rectangle-4" />
-            <div className="remember-me">Remember Me? </div>
+          </div>
+        </nav>
+{/* News feed */}
+<div class="col-12 col-sm-6 col-md-8 float-right d-flex justify-content-center sticky-top">
+  <div class="p-2">News Feed</div>
+  <div class="p-2">News Feed</div>
+  <div class="p-2">News Feed</div>
+  <div class="p-2">News Feed</div>
+    </div>
+
+        <form> {/* Email and Password */}
+        <div class="form-group">
+          <h2 class="above">Log in to OutReachD</h2>
+          <input type="email" class="form-control shadow p-3 mb-5 bg-light rounded rectangle-1" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <input type={isPasswordShown ? "text" : "password"} class="form-control shadow p-3 mb-5 bg-light rounded rectangle-2" id="exampleInputPassword1" placeholder="Password"/>
+            <i className={`fa ${isPasswordShown ? "fa-eye" : "fa-eye-slash"} password-icon`} onClick={this.togglePasswordVisiblity} />
+    </div>
+    
+    <div class="form-check mb-2 mr-sm-2">
+    <input class="form-check-input" type="checkbox" id="inlineFormCheck"/>
+    <label class="form-check-label remember"for="inlineFormCheck">
+      Remember me?
+    </label>
+  </div>
+  <Link to={"./NFPList"}>
+  <button type="button" class="btn btn-primary rectangle-3">Log In</button></Link>
+  </form>
+ 
             <Link to={"./signup"}>
               <div className="new-to-outreachd-sign-up-now">New to OutReachD?  Sign up now</div>
             </Link>
-            <svg preserveAspectRatio="none" viewBox="-0.75 -0.75 292.5 456.5" className="path-1">
+  
 
-            </svg>
-            <div className="your-privacy-is-important-to-us-well-never-share-your-information"> Your privacy is important to us, we'll never share<br /> your information.</div>
 
-            <Link to={"./noPage"}>
-              <div className="fogot">Forgot?</div></Link>
+
+{/* Background image.   Broken. */}
+{/*<img src="assets/image-1.png"></img>
+  <div class="float-right"><img src="assets/image-1.png" class="img-fluid" alt="Responsive image"/>
+Preview
+Float right on all viewport sizes</div>
+    */}
+
           </div>
-        </div>
+          )
+        }
+      }
       
-
-    );
-  }
-}
+         
+      
+      
+      
+     
